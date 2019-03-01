6a6880847011267e913c96a856a7ce239d118844
OMGCA <41295316+OMGCA@users.noreply.github.com>
2019-03-01 17:38:37 +0000

    Server: increased readability, deprecated unused variables and methods

--- src/server/Server.java
+++ src/server/Server.java
@@ -33,13 +33,10 @@ public class Server {
 	private static ServerSocket modSocket = null;
 	private static Socket mSocket = null;
 
-	private static ServerSocket addSocket = null;
-	private static Socket aSocket = null;
-
 	/* 定义病人查询和修改/新增使用的网络端口 */
 	private static int queryPort = 34167;
 	private static int modPort = 34168;
-	private static int addPort = 34169;
+
 	/* 定义病人类列表 */
 	private static List<Patient> patientList = null;
 	/* 数据库文件位置 */
@@ -69,15 +66,17 @@ public class Server {
 				try {
 					/* 初始化ServerSocket类 */
 					serverSocket = new ServerSocket(queryPort);
-					writeToLog(logFile,"Port "+queryPort+" is opened.");
+					writeToLog(logFile, "Port " + queryPort + " is opened.");
 					System.out.println("等待客户端请求");
 					/* 等待客户端连接 */
 					socket = serverSocket.accept();
+
 					/* 从数据库导入病人信息 */
 					importPatientInfo();
 					String clientAddress = socket.getRemoteSocketAddress().toString();
 					System.out.println("客户端" + clientAddress + " 已连接，端口 " + queryPort);
-					writeToLog(logFile,clientAddress+" has connected to server");
+					writeToLog(logFile, clientAddress + " has connected to server");
+
 					/* 定义InputStream，此方法中从客户端得到字段的输入，故使用InputStream */
 					InputStream inStream = null;
 					/* 得到客户端发来的字段 */
@@ -92,14 +91,14 @@ public class Server {
 					}
 					/* 通过接收到的从客户端发来的字段，筛选出将要发送至客户端的病人信息 */
 					Patient patient = queryPatient(sb.toString());
-					writeToLog(logFile,clientAddress+" has initialized a request on info of patient "+ sb);
+					writeToLog(logFile, clientAddress + " has initialized a request on info of patient " + sb);
 					System.out.println("收到NFC标签" + sb + "的信息请求");
 
 					/* 定义ObjectOutputStream，输出为发送至客户端的Patient类，故使用ObjectOutputStream */
 					ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
 					/* 将第82行中筛选出的病人信息写入outStream */
 					outStream.writeObject(patient);
-					writeToLog(logFile,"Info of patient " + sb + " has been sent to "+clientAddress);
+					writeToLog(logFile, "Info of patient " + sb + " has been sent to " + clientAddress);
 					System.out.println("已将患者" + patient.getName() + "信息发送至客户端");
 					System.out.println("-------------------");
 
@@ -122,18 +121,20 @@ public class Server {
 				try {
 					/* 初始化ServerSocket类 */
 					modSocket = new ServerSocket(modPort);
-					writeToLog(logFile,"Port "+modPort+" is opened.");
+					writeToLog(logFile, "Port " + modPort + " is opened.");
 					System.out.println("等待新的患者信息发送");
 					mSocket = modSocket.accept();
+
 					/* 等待客户端接入 */
 					String clientAddress = socket.getRemoteSocketAddress().toString();
 					System.out.println("客户端" + mSocket.getRemoteSocketAddress().toString() + "已连接至端口" + modPort);
-					writeToLog(logFile,clientAddress+" has connected to server");
+					writeToLog(logFile, clientAddress + " has connected to server");
+
 					/* 修改/新增病人需要接收客户端发送的Patient类，故使用ObjectInputStream */
 					ObjectInputStream inStream = new ObjectInputStream(mSocket.getInputStream());
 					/* 新建本地Patient类，存储客户端发送的Patient类 */
 					Patient p = (Patient) inStream.readObject();
-					writeToLog(logFile,clientAddress+" is attempting to modify info of patient " + p.getSlotID());
+					writeToLog(logFile, clientAddress + " is attempting to modify info of patient " + p.getSlotID());
 					System.out.println(
 							"客户端" + mSocket.getRemoteSocketAddress().toString() + "已修改病人" + p.getName() + "信息");
 					System.out.println("-------------------");
@@ -159,13 +160,15 @@ public class Server {
 		}
 	}
 
-	public static void writeToLog(File logFile, String contentType){
+	/* 事件写入外部日志方法 */
+	public static void writeToLog(File logFile, String contentType) {
+		/* 获得时间戳 */
 		String timestamp = new SimpleDateFormat("yyyy-MM-dd_HH_mm_ss").format(new Date());
-		try{
+		try {
 			Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(logFile, true), "UTF-8"));
-			
-			writer.write("["+timestamp+"]"+"\t");
-			writer.write(contentType+"\n");
+
+			writer.write("[" + timestamp + "]" + "\t");
+			writer.write(contentType + "\n");
 
 			writer.flush();
 			writer.close();
@@ -224,29 +227,33 @@ public class Server {
 		/* 垃圾回收，否则接下来的代码无法执行 */
 		System.gc();
 
-		if (originalDatabase.delete()){
+		/* 删除原始数据库 */
+		if (originalDatabase.delete()) {
 			System.out.println("正在更新数据库");
-			writeToLog(logFile,"Updating database");
-		}
-			
-		else{
-			System.out.println("DATABASE_DELETE_ERROR: Fail to delete original database");
-			writeToLog(logFile,"DATABASE_DELETE_ERROR: Fail to delete original database");
+			writeToLog(logFile, "Updating database");
 		}
 
-		if (fTmp.renameTo(originalDatabase)){
+		else {
+			System.out.println("DATABASE_DELETE_ERROR: Fail to delete original database");
+			writeToLog(logFile, "DATABASE_DELETE_ERROR: Fail to delete original database");
+		}
+
+		/* 重命名临时数据库 */
+		if (fTmp.renameTo(originalDatabase)) {
 			String success = "病人信息修改成功";
 			System.out.println("数据库更新完成");
-			writeToLog(logFile,"Database has been updated");
-			writeToLog(logFile,"Info of patient " + p.getSlotID() + " has been successfully modified by "+clientAddress);
+			writeToLog(logFile, "Database has been updated");
+			writeToLog(logFile,
+					"Info of patient " + p.getSlotID() + " has been successfully modified by " + clientAddress);
 			return success;
 		}
-			
-		else{
+
+		else {
 			String error = "DATABASE_UPDATE_ERROR: Fail to rename tmp database";
 			System.out.println(error);
 			writeToLog(logFile, error);
-			writeToLog(logFile,"Info of patient " + p.getSlotID() + " modification operation has failed, attempted by "+clientAddress);
+			writeToLog(logFile, "Info of patient " + p.getSlotID() + " modification operation has failed, attempted by "
+					+ clientAddress);
 			return error;
 		}
 	}
@@ -315,7 +322,6 @@ public class Server {
 		/* 线性搜索patientList */
 		for (int i = 0; i < patientList.size(); i++) {
 			/* 当查询的ID符合某行病人ID时返回该病人，筛选结束 */
-			/* 由于修改病人方法尚未完成，在有多个重复ID病人信息在数据库的情况下，会返回第一个 */
 			if (patientList.get(i).getSlotID().equals(ID)) {
 				return patientList.get(i);
 			}
